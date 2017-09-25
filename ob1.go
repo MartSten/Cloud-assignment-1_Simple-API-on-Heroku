4906c575d06b27d30e13d911d243e0aa3ed23889
martin <martsten@live.no>
2017-09-25 09:17:34 +0200

    I don't know if this fixed it, but I tried - govet

--- ob1.go
+++ ob1.go
@@ -1,68 +1,72 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strings"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 )
-
 
 /**
 Function for handling of the url.
- */
-func urlHandler(w http.ResponseWriter, r *http.Request){
+
+*/
+
+func urlHandler(w http.ResponseWriter, r *http.Request) {
 
 	//framed code form: https://stackoverflow.com/questions/31622052/how-to-serve-up-a-json-response-using-go/31622112
 	//-------------------------------------------------------------
-	w.Header().Set("Content-Type", "application/json")	//sets content-type so that user clients will know to expect json
+
+	w.Header().Set("Content-Type", "application/json") //sets content-type so that user clients will know to expect json
 	//-------------------------------------------------------------
 
 	//Function variables
-	var apiContributor string	//The url for the contributors api
-	var apiLanguage string	//The url for the languages api
-	var url = string(r.URL.Path)	//gets the url fom r (the http request) and casts it to string
 
-	var projectData = strings.Split(url, "/")	//gets the name and owner of a github project
-	if len(projectData) != 6 {	//if the incorrect url is requested: send error.
+	var apiContributor string    //The url for the contributors api
+	var apiLanguage string       //The url for the languages api
+	var url = string(r.URL.Path) //gets the url fom r (the http request) and casts it to string
+
+	var projectData = strings.Split(url, "/") //gets the name and owner of a github project
+	if len(projectData) != 6 {                //if the incorrect url is requested: send error.
 		http.Error(w, "incorrect url. needs 5 parameters", 400)
 		return
 	}
 
-	apiContributor= "https://api." + projectData[3] + "/repos/" + projectData[4] + "/" + projectData[5] + "/contributors"
+	apiContributor = "https://api." + projectData[3] + "/repos/" + projectData[4] + "/" + projectData[5] + "/contributors"
 
-	apiLanguage= "https://api." + projectData[3] + "/repos/" + projectData[4] + "/" + projectData[5] + "/languages"
+	apiLanguage = "https://api." + projectData[3] + "/repos/" + projectData[4] + "/" + projectData[5] + "/languages"
 
-	pOwner := projectData[4]	//project owner
-	theProject := projectData[5]	//project name
-	var topCom string	//top contributor
-	var amountOfComs int	//top contributor's amount of commits
-	var badRequest bool		//used when there is a bad request
+	pOwner := projectData[4]     //project owner
+	theProject := projectData[5] //project name
+	var topCom string            //top contributor
+	var amountOfComs int         //top contributor's amount of commits
+	var badRequest bool          //used when there is a bad request
 
 	theContributors := getContributor(apiContributor)
-	if len(theContributors) < 1 {	//if there were errors getting the contributor data
+	if len(theContributors) < 1 { //if there were errors getting the contributor data
 		fmt.Println("Could not find the correct api (contributor) data. Is the url correct?")
 		topCom = "No committers found"
 		amountOfComs = 0
 		badRequest = true
-	} else {	//If there were no errors getting the contributor data
+	} else { //If there were no errors getting the contributor data
 		topCom = theContributors[0].Committer
 		amountOfComs = theContributors[0].Commits
 	}
 
 	theLanguages := getLanguage(apiLanguage)
-	if len(theLanguages.Language) < 1{
+	if len(theLanguages.Language) < 1 {
 		fmt.Println("Could not find the correct api (language) data. Is the url correct?")
 		theLanguages.Language = append(theLanguages.Language, "No languages fund")
 		badRequest = true
 	}
 
 	//Struct for sending the final json payload
+
 	if badRequest == true {
 		http.Error(w, "Bad request", 400)
-	}else {
+	} else {
 		theResponse := finalStruct{
 			Project:   theProject,
 			Owner:     pOwner,
@@ -76,11 +80,10 @@ func urlHandler(w http.ResponseWriter, r *http.Request){
 
 }
 
-
 /**
 GET /repos/:owner/:repo/languages
 A struct that holds the repo's language data
- */
+*/
 
 type languages struct {
 	Language []string
@@ -91,25 +94,29 @@ function getLanguage()
 The function gets a github repo's languages based on an url to the github api
 PARAM: The function takes an url to a github repo's language api
 RETURN: On success the function returns an Language slice containing languages. On failure the function returns an empty Language{}
- */
+*/
+
 func getLanguage(urlToAPI string) languages {
 
-	resp, err := http.Get(urlToAPI)	//Gets the api's content based on url
-	if err != nil{
+	resp, err := http.Get(urlToAPI) //Gets the api's content based on url
+	if err != nil {
 		//http.Error(writer, http.StatusText(400), 400)
+
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 
 	//Done with request. Closing it.
+
 	defer resp.Body.Close()
 
-	var a interface{}	//create interface a
+	var a interface{} //create interface a
 
 	lang := languages{}
 
 	//Process languages by unmarshaling them, and puts them inn to a
-	err =json.Unmarshal(body, &a)
+
+	err = json.Unmarshal(body, &a)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Could not find the correct api (languages) data. Is the url correct?")
@@ -117,12 +124,14 @@ func getLanguage(urlToAPI string) languages {
 	}
 	//fmt.Println(language)
 
-	m := a.(map[string]interface{})		//map a
+	m := a.(map[string]interface{}) //map a
 	//for each a - put it inn to languages through lang.Language
+
 	for k := range m {
 		lang.Language = append(lang.Language, k)
 	}
 	//fmt.Println(lang)
+
 	if lang.Language[0] == "message" {
 		fmt.Println("Could not find the correct api (languages) data. Is the github-url correct?")
 		return languages{}
@@ -135,10 +144,11 @@ func getLanguage(urlToAPI string) languages {
 GET /repos/:owner/:repo/contributors
 A struct that holds the repo's contributor data
 based on the github api
- */
+*/
+
 type contributors []struct {
 	Committer string `json:"login"`
-	Commits int `json:"contributions"`
+	Commits   int    `json:"contributions"`
 }
 
 /**
@@ -147,34 +157,39 @@ the function gets a github repo's contributors from the github api.
 it then processes them and returns them as an array
 PARAM: the function takes the url for the github api as it's parameter
 RETURN: on success it returns an array of contributors. On failure it returns an empty contributor
- */
+*/
+
 func getContributor(urlToAPI string) contributors {
 
 	resp, err := http.Get(urlToAPI)
-	if err != nil{
+	if err != nil {
 		//http.Error(writer, http.StatusText(400), 400)
+
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 
 	//Done with request. Closing it.
+
 	defer resp.Body.Close()
 
 	//Process json fom the api
+
 	topCon := contributors{}
-	err =json.Unmarshal(body, &topCon)
-	if err != nil {		//if the json can't be placed in contributors, see if it can be placed in apiError
+	err = json.Unmarshal(body, &topCon)
+	if err != nil { //if the json can't be placed in contributors, see if it can be placed in apiError
 		fmt.Println(err)
 		errorAPI := apiError{}
 		err2 := json.Unmarshal(body, &errorAPI)
-		if err2 != nil {	//if the json can't be placed in apiError
-				fmt.Println(err2)
+		if err2 != nil { //if the json can't be placed in apiError
+			fmt.Println(err2)
 			return contributors{}
-			}
+		}
 		fmt.Println("Could not find the correct api data. Is the url correct?")
 		return contributors{}
 	}
 	//fmt.Println(topCon)
+
 	return topCon
 }
 
@@ -182,28 +197,30 @@ func getContributor(urlToAPI string) contributors {
 apiError struct
 Holds data fom the github api's error message
 Used when the script fails to get data fom a given api url
- */
-type apiError struct{
-	Message string `json:"message"`
+*/
+
+type apiError struct {
+	Message          string `json:"message"`
 	DocumentationURL string `json:"documentation_url"`
 }
 
 /**
 The struct finalStruct holds the data which is to be presented as json to the user
- */
-type finalStruct struct{
-	Project string
-	Owner string
-	Committer string
-	Commits int
-	Language []string
-}
+*/
 
+type finalStruct struct {
+	Project   string
+	Owner     string
+	Committer string
+	Commits   int
+	Language  []string
+}
 
 /**
 Function main
 Does main things
- */
+*/
+
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/projectinfo/v1/", urlHandler)
@@ -212,11 +229,13 @@ func main() {
 
 	//framed code from: https://www.youtube.com/watch?v=IWT2vCbTURs&t=332s
 	//---------------------------------------------------------------------
+
 	if port == "" {
 		port = "8080"
 		fmt.Println("No port detected. Port has been set to 8080")
 	}
 	//---------------------------------------------------------------------
+
 	http.ListenAndServe(":"+port, nil)
 
 }
